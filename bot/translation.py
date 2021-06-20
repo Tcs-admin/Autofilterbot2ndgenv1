1e36dede9188fa92d84fec605a3a85c7eb58b288
Tcs-admin <86108663+Tcs-admin@users.noreply.github.com>
2021-06-20 22:32:16 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,14 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hey {} How Are You!!</b>
+<i>I am an Advanced Filter Bot 😎...
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+I will find movies for you from my archive 🗃 ...
+You can't add me to your group. Try me by joining @telecinemas and @tcseries 🤩
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@telecinemas
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +44,10 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@telecinemas
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
-
 <b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
